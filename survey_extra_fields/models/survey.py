56c73b60e98fa5fd7f3b2549569f8c6d6cfc9353
Miracle Ayodele <miracleaayodele@gmail.com>
2021-02-20 19:12:32 +0100

    survey mail template update

--- survey_extra_fields/models/survey.py
+++ survey_extra_fields/models/survey.py
@@ -4,10 +4,20 @@ from odoo import models, fields, api, _
 class Survey(models.Model):
     _inherit = 'survey.survey'
 
-    user_id = fields.Many2one('res.users', string="User", default=lambda self: self.env.user)
+    user_id = fields.Many2one('res.users', string="User", compute='_user_id')
+
+    @api.depends('title')
+    def _user_id(self):
+        for rec in self:
+            rec.user_id = self.env.user.id
 
 
 class SurveyUser(models.Model):
     _inherit = 'survey.user_input'
 
-    user_id = fields.Many2one('res.users', string="User", default=lambda self: self.env.uid)
+    user_id = fields.Many2one('res.users', string="User", compute='_user_id')
+
+    @api.depends('partner_id')
+    def _user_id(self):
+        for rec in self:
+            rec.user_id = self.env.user.id
